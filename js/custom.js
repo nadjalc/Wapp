54074ead730dbee4cf0c933aed8c78a20cbef98e
Nadja <nadjajelicic9@gmail.com>
2015-12-10 01:52:05 +0100

    prvo
    nesto

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+function showToast () {
+   if(typeof Windows !== undefined) {
+     var notifications = Windows.UI.Notifications;
+     var template = notifications.ToastTemplateType.toastImageAndText01;
+     var toastXml = notifications.ToastNotificationManager.getTemplateContent(template);
+     var toastTextElements = toastXml.getElementsByTagName("text");
+     toastTextElements[0].appendChild(toastXml.createTextNode("Toast from Codepen"));
+     var toastImageElements = toastXml.getElementsByTagName("image");
+     toastImageElements[0].setAttribute("src", "http://assets.codepen.io/assets/social/facebook-default.png");
+     toastImageElements[0].setAttribute("alt", "red graphic");
+     var toast = new notifications.ToastNotification(toastXml);
+     var toastNotifier = notifications.ToastNotificationManager.createToastNotifier();
+     toastNotifier.show(toast);
+   }
+}
+
+function  systemAlertCommandInvokedHandler() {
+  console.log ("OUTPUT: The " + command.label + " was selected");
+}
